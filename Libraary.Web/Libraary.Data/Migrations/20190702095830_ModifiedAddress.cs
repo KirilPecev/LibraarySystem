e7aabf8ae0d244619857b687a0ec69fb0b5fab04
Kiril Pecev <kir4o_pecev@abv.bg>
2019-07-02 13:02:10 +0300

    added new migration for address (Added Zip property)

---
+++ Libraary.Web/Libraary.Data/Migrations/20190702095830_ModifiedAddress.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Libraary.Data.Migrations
+{
+    public partial class ModifiedAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Zip",
+                table: "Addresses",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Zip",
+                table: "Addresses");
+        }
+    }
+}
