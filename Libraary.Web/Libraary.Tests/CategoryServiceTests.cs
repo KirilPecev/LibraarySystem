64ded662f45e8db786563551841ccdd99884f3f3
Kiril Pecev <kir4o_pecev@abv.bg>
2019-08-09 22:16:25 +0300

    Added Tests for Category service

---
+++ Libraary.Web/Libraary.Tests/CategoryServiceTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Libraary.Tests
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using Common;
+    using Services;
+    using Services.DTOs.Publisher;
+    using Xunit;
+
+    public class CategoryServiceTests
+    {
+        private ICategoryService categoryService;
+
+        [Fact]
+        public void GetCategories_ShouldReturnAllCategories()
+        {
+            string errorMessagePrefix = "CategoryService GetCategories() method does not work properly.";
+
+            var context = LibraaryDbContextInMemoryFactory.InitializeContext();
+            this.categoryService = new CategoryService(context);
+            LibraaryDbContextInMemoryFactory.SeedDb(context);
+
+            var categories = this.categoryService.GetCategories();
+            Assert.True(categories.Count() != 0, errorMessagePrefix);
+        }
+
+        [Fact]
+        public void GetCategory_WithCorrectData_ShouldReturnCategory()
+        {
+            string errorMessagePrefix = "CategoryService GetCategory() method does not work properly.";
+
+            var context = LibraaryDbContextInMemoryFactory.InitializeContext();
+            this.categoryService = new CategoryService(context);
+            LibraaryDbContextInMemoryFactory.SeedDb(context);
+
+            var category = this.categoryService.GetCategory("Fantasy");
+            Assert.True(category != null, errorMessagePrefix);
+        }
+
+        [Fact]
+        public void GetCategory_WithIncorrectData_ShouldReturnCategory()
+        {
+            string errorMessagePrefix = "CategoryService GetCategory() method does not work properly.";
+
+            var context = LibraaryDbContextInMemoryFactory.InitializeContext();
+            this.categoryService = new CategoryService(context);
+
+            var category = this.categoryService.GetCategory("Fantasyss");
+            Assert.True(category == null, errorMessagePrefix);
+        }
+    }
+}
